667a330f426cd4034c20b59f4845f9c8a5bed41d
Vitalii Omelchenko <vitalii.omelchenko@materialise.kiev.ua>
2021-07-18 21:34:49 +0300

    Add project files.

---
+++ NewMatrix/Matrix.h
@@ -0,0 +1,328 @@
+#pragma once
+#pragma once
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <array>
+#include <type_traits>
+
+
+
+template <typename T, size_t N, size_t M>
+class Matrix {
+private:
+    std::array<T, N* M> m_matrix;
+public:
+
+    Matrix() = default;
+    ~Matrix() = default;
+
+    size_t Height() const;
+    size_t Width() const;
+
+    T operator[] (size_t i) const;
+    T& operator[] (size_t i);
+
+    template <typename = std::enable_if_t<N == M>>
+    T  Determinant () const;
+
+    //operation with scalars
+    template <typename T2, typename = std::enable_if_t<std::is_arithmetic_v<T2>>>
+    auto operator+ (const T2 add);
+
+    template <typename T2, typename = std::enable_if_t<std::is_arithmetic_v<T2>>>
+    auto operator- (const T2 add);
+
+    template <typename T2, typename = std::enable_if_t<std::is_arithmetic_v<T2>>>
+    auto operator* (const T2 add);
+
+    template <typename T2, typename = std::enable_if_t<std::is_arithmetic_v<T2>>>
+    auto operator/ (const T2 add);
+
+    //template <typename T2>
+    //friend auto operator= (const T2 add);
+
+    //unitary operation 
+    Matrix<T, N, M>& operator++ (); //prefix
+
+    Matrix<T, N, M> operator++ (int); //postfix
+
+    Matrix<T, N, M>& operator-- (); //prefix
+
+    Matrix<T, N, M> operator-- (int); //postfix
+
+    //binary operation between matrix
+    template <typename T2>
+    auto operator+ (const Matrix<T2, N, M>& add);
+
+    template <typename T2>
+    auto operator- (const Matrix<T2, N, M>& sub);
+
+    template<typename T2, size_t M2>
+    auto operator* (const Matrix<T2, M, M2>& mult);
+
+
+    Matrix<T, N, M>& operator= (const Matrix<T, N, M>& copy);
+};
+
+
+
+template <typename T, size_t N, size_t M>
+size_t Matrix<T, N, M>::Height() const
+{
+    return N;
+}
+template <typename T, size_t N, size_t M>
+size_t Matrix<T, N, M>::Width() const
+{
+    return M;
+}
+
+template <typename T, size_t N, size_t M>
+template <typename>
+T  Matrix<T, N, M>::Determinant() const
+{
+    if constexpr (N == 1)
+    {
+        std::cout << "Determinant for N=M = 1\n";
+        //return (m_matrix[0] < 0 ? (-m_matrix[0]) : m_matrix[0]);
+        return  m_matrix[0];
+    }
+    else if (N == 2)
+    {
+        std::cout << "Determinant for N=M = 2\n";
+        return m_matrix[0] * m_matrix[3] - m_matrix[1] * m_matrix[2];
+    }
+    else if (N == 3)
+    {
+        std::cout << "Determinant for N=M = 3\n";
+        return (m_matrix[0] * m_matrix[4] * m_matrix[8] + 
+            m_matrix[1] * m_matrix[5] * m_matrix[6] +
+            m_matrix[2] * m_matrix[3] * m_matrix[7]) -
+            (m_matrix[0] * m_matrix[5] * m_matrix[7] +
+            m_matrix[1] * m_matrix[3] * m_matrix[8] +
+            m_matrix[2] * m_matrix[4] * m_matrix[6]);
+    }
+    else
+    {
+        std::cout << "Determinant for N=M >= 4 (Has no realisation)\n";
+        return  static_cast<T>(0);
+    }
+}
+
+//operation with scalars
+template <typename T, size_t N, size_t M>
+template <typename T2, typename>
+auto Matrix<T, N, M>::operator+ (const T2 number)
+{
+    using resultType = decltype(std::declval<T>() + std::declval<T2>());
+    Matrix<resultType, N, M> result;
+    for (int i = 0; i < N * M; ++i)
+    {
+        result[i] = this->m_matrix[i] + number;
+    }
+
+    return result;
+}
+
+template <typename T, size_t N, size_t M>
+template <typename T2, typename>
+auto Matrix<T, N, M>::operator- (const T2 number)
+{
+    using resultType = decltype(std::declval<T>() - std::declval<T2>());
+    Matrix<resultType, N, M> result;
+    for (int i = 0; i < N * M; ++i)
+    {
+        result[i] = this->m_matrix[i] - number;
+    }
+
+    return result;
+}
+
+template <typename T, size_t N, size_t M>
+template <typename T2, typename>
+auto Matrix<T, N, M>::operator* (const T2 number)
+{
+    using resultType = decltype(std::declval<T>()* std::declval<T2>());;
+    Matrix<resultType, N, M> result;
+    for (int i = 0; i < N * M; ++i)
+    {
+        result[i] = this->m_matrix[i] * number;
+    }
+
+    return result;
+}
+
+template <typename T, size_t N, size_t M>
+template <typename T2, typename>
+auto Matrix<T, N, M>::operator/ (const T2 number)
+{
+    using resultType = decltype(std::declval<T>() / std::declval<T2>());
+    Matrix<resultType, N, M> result;
+    for (int i = 0; i < N * M; ++i)
+    {
+        result[i] = this->m_matrix[i] / number;
+    }
+
+    return result;
+}
+
+//template <typename T2>
+//auto operator= (const T2 number);
+
+
+//unitary operation 
+template <typename T, size_t N, size_t M>
+Matrix<T, N, M>& Matrix<T, N, M>::operator++ () ////prefix
+{
+    for (int i = 0; i < N * M; ++i)
+    {
+        this->m_matrix[i]++;
+    }
+
+    return *this;
+}
+
+template <typename T, size_t N, size_t M>
+Matrix<T, N, M> Matrix<T, N, M>::operator++ (int) //postfix
+{
+    Matrix<T, N, M> temp = *this;
+    for (int i = 0; i < N * M; ++i)
+    {
+        this->m_matrix[i]++;
+    }
+
+    return temp;
+}
+
+template <typename T, size_t N, size_t M>
+Matrix<T, N, M>& Matrix<T, N, M>::operator-- () //prefix
+{
+    for (int i = 0; i < N * M; ++i)
+    {
+        this->m_matrix[i]--;
+    }
+
+    return *this;
+}
+
+template <typename T, size_t N, size_t M>
+Matrix<T, N, M> Matrix<T, N, M>::operator-- (int) //postfix
+{
+    Matrix<T, N, M> temp = *this;
+    for (int i = 0; i < N * M; ++i)
+    {
+        this->m_matrix[i]--;
+    }
+
+    return temp;
+}
+
+//binary operation between matrix
+template <typename T, size_t N, size_t M>
+template <typename T2>
+auto Matrix<T, N, M>::operator+ (const Matrix<T2, N, M>& add)
+{
+    using resultType = decltype(std::declval<T>() + std::declval<T2>());
+    Matrix<resultType, N, M> result;
+
+    for (int i = 0; i < N * M; ++i)
+        result[i] = this->m_matrix[i] + add[i];
+
+    return result;
+}
+
+template <typename T, size_t N, size_t M>
+template <typename T2>
+auto Matrix<T, N, M>::operator- (const Matrix<T2, N, M>& sub)
+{
+    using resultType = decltype(std::declval<T>() - std::declval<T2>());
+    Matrix<resultType, N, M> result;
+
+    for (int i = 0; i < N * M; ++i)
+        result[i] = this->m_matrix[i] - sub[i];
+
+    return result;
+}
+
+
+template<typename T, size_t N, size_t M>
+template<typename T2, size_t M2>
+auto Matrix<T, N, M>::operator*(const Matrix<T2, M, M2>& mult)
+{
+    using resultType = decltype(std::declval<T>()* std::declval<T2>());;
+
+    Matrix<resultType, N, M2> result;
+
+    resultType temp;
+    for (size_t i = 0; i < N; i++)
+    {
+        for (size_t j = 0; j < M2; j++)
+        {
+            temp = 0;
+            for (size_t k = 0; k < M; k++)
+            {
+                T temp1 = this->m_matrix[i * M + k];
+                T2 temp2 = mult[k * M2 + j];
+                temp += this->m_matrix[i * M + k] * mult[k * M2 + j];
+            }
+            result[i * M2 + j] = temp;
+        }
+    }
+    return result;
+}
+
+template <typename T, size_t N, size_t M>
+Matrix<T, N, M>& Matrix<T, N, M>::operator= (const Matrix<T, N, M>& copy)
+{
+    this->m_matrix = copy.m_matrix;
+    return *this;
+}
+
+template <typename T, size_t N, size_t M>
+T  Matrix<T, N, M>::operator[](size_t i) const
+{
+    return m_matrix[i];
+}
+
+template <typename T, size_t N, size_t M>
+T& Matrix<T, N, M>::operator[](size_t i)
+{
+    return m_matrix[i];
+}
+
+
+
+template <typename T, size_t N, size_t M>
+std::ostream& operator<<(std::ostream& os, const Matrix<T, N, M>& out)
+{
+    if constexpr (N == 1)
+    {
+        os << "Output for N = 1\n";
+        for (size_t j = 0; j < M; j++)
+            os << out[j] << ' ';
+        os << '\n';
+    }
+    else if (M == 1)
+    {
+        os << "Output for M = 1\n";
+        for (size_t i = 0; i < N; i++)
+            os << out[i] << '\n';
+    }
+    else
+    {
+      //  os << "Output for else\n";
+        for (size_t i = 0; i < N; i++)
+        {
+            for (size_t j = 0; j < M; j++)
+                os << out[i * M + j] << ' ';
+            os << '\n';
+        }
+    }
+    
+
+    return os;
+}
+
+
---
+++ NewMatrix/main.cpp
@@ -0,0 +1,56 @@
+#include "Matrix.h"
+
+#include <iostream>
+#include <type_traits>
+
+
+int main()
+{
+    constexpr int n = 2;
+    constexpr int m = 3;
+
+    Matrix<int, n, m> a;
+    Matrix<float, n, m> b;
+    Matrix<double, m, n> c;
+
+    for (size_t i = 0; i < a.Height(); i++)
+        for (size_t j = 0; j < a.Width(); j++)
+            a[i * a.Width() + j] = 1;
+
+    for (size_t i = 0; i < b.Height(); i++)
+        for (size_t j = 0; j < b.Width(); j++)
+            b[i * b.Width() + j] = static_cast<float>(1.f * ((i + 1) + (j + 5)) / ((i % (j+1)) + 1));
+
+    for (size_t i = 0; i < c.Height(); i++)
+        for (size_t j = 0; j < c.Width(); j++)
+            c[i * c.Width() + j] = static_cast<double>((i + 1) % (j + 3));
+
+    std::cout << "----------------------------" << std::endl
+             << "a =\n" << a << std::endl
+             << "a++ =\n" << a++ << std::endl
+             << "++a =\n" << ++a << std::endl
+             << "a + 0.5f =\n" << a+0.5f << std::endl
+             << "a - 0.1f =\n" << a - 0.1f << std::endl
+             << "a * 2 =\n" << a * 2 << std::endl
+             << "a / 2.f =\n" << a / 2.f << std::endl;
+
+    std::cout << "----------------------------" << std::endl
+            << "a =\n" << a << std::endl
+            << "b =\n" << b << std::endl
+            << "a + b =\n" << a + b << std::endl
+            << "a - b =\n" << a - b << std::endl;
+
+
+    for (size_t i = 0; i < a.Height(); i++)
+        for (size_t j = 0; j < a.Width(); j++)
+            a[i * a.Width() + j] = static_cast<int>((j+i) % (i + 6));
+    auto d = c * a;
+    std::cout << "----------------------------" << std::endl
+           << "a =\n" << a << std::endl
+           << "c =\n" << c << std::endl
+           << "d = c*a =\n" << d << std::endl;
+
+    std::cout << "----------------------------" << std::endl
+        << "Determinant of d: \n" << d.Determinant() << std::endl;
+    return 0;
+}
